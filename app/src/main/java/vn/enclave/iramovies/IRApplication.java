d3b27e87be32e147148e618de94344cc798b8355
lorence <lorence@enclave.vn>
2018-01-12 10:36:13 +0700

    Implement ver35

--- app/src/main/java/vn/enclave/iramovies/IRApplication.java
+++ app/src/main/java/vn/enclave/iramovies/IRApplication.java
@@ -2,11 +2,45 @@ package vn.enclave.iramovies;
 
 import android.app.Application;
 
+import io.branch.referral.Branch;
 import vn.enclave.iramovies.services.IraMovieWebAPIs;
 
 /**
  *
  * Created by lorence on 14/11/2017.
+ */
+
+/**
+ * My First Link: https://nmrv.test-app.link/IjRIrfMpDJ
+ * - Configure link:
+ *  - Link Settings https://exampleurl.com/iramovie.apk
+ *  - Quick Link:
+ *    - Deep View: Android: branch_default [If App not installed => show URL(button to get Application => Open new URL)]
+ *
+ *  Case 1:
+ *  - Emulator: Application already installed -> Running URL => Open GUI of Deep View ((Web browser))
+ *  - Sony Xperia XA: Application already installed => Running URL => Open GUI of Deep View (branch default)
+ *
+ *      ---> Click Button Get The App (1> Open Application in device 2> Run url https://exampleurl.com/iramovies.apk)
+ *
+ *  Case 2: Uninstall Application
+ *  - Emulator: Running URL => Open DeepView => Get The App => The site can't be reached
+ *  - Sony Xperia XA: Running URL => Open DeepView => Get The App => Item not found. (Play Store)
+ *
+ *  If we configure URL in Link Settings => Launch URL => Running Settings. Or Set Web URL (Running this link)
+ */
+
+
+/**
+ * My Second Link: https://nmrv.test-app.link/RFOzTN8tDJ
+ * - Quick Link:
+ *    - Deep View: Android: Default Redirect https://exampleurl.com/iramovies.apk (Get it from Link Settings)
+ *
+ * Case 1: Already installed application
+ *  - Emulator: (Running Application or Running URL without DeepView)
+ *  - Sony Xperia XA:
+ *
+ * Case 2: Already uninstall application
  */
 
 public class IRApplication extends Application{
@@ -19,6 +53,15 @@ public class IRApplication extends Application{
      * EzFaxingWebAPIs
      */
     private IraMovieWebAPIs mIraMoviesWebAPIs;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Initialize the Branch object
+        Branch.getAutoInstance(this);
+
+    }
 
     public static synchronized IRApplication getInstance() {
         if (sInstance == null) {
--- app/src/main/java/vn/enclave/iramovies/ui/activities/base/Splash/SplashScreen.java
+++ app/src/main/java/vn/enclave/iramovies/ui/activities/base/Splash/SplashScreen.java
@@ -5,12 +5,17 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
+import io.branch.indexing.BranchUniversalObject;
+import io.branch.referral.Branch;
+import io.branch.referral.BranchError;
+import io.branch.referral.util.LinkProperties;
 import vn.enclave.iramovies.R;
 import vn.enclave.iramovies.ui.activities.base.BaseView;
 import vn.enclave.iramovies.ui.activities.base.Home.HomeView;
@@ -33,7 +38,7 @@ public class SplashScreen extends BaseView {
     @Override
     public void activityCreated(Bundle savedInstanceState) {
         hiddenStatusBar();
-        btnsub = (Button)findViewById(R.id.buttonsub);
+        btnsub = findViewById(R.id.buttonsub);
         l1 = findViewById(R.id.l1);
         l2 = findViewById(R.id.l2);
         uptodown = AnimationUtils.loadAnimation(this,R.anim.uptodown);
@@ -56,5 +61,29 @@ public class SplashScreen extends BaseView {
                 | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                 | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Branch branch = Branch.getInstance();
+
+        branch.initSession(new Branch.BranchUniversalReferralInitListener() {
+            @Override
+            public void onInitFinished(BranchUniversalObject branchUniversalObject, LinkProperties linkProperties, BranchError error) {
+                if (error == null) {
+                    // params are the deep linked params associated with the link that the user clicked -> was re-directed to this app
+                    // params will be empty if no data found
+                    // ... insert custom logic here ...
+                } else {
+                    Log.i("MyApp", error.getMessage());
+                }
+            }
+        }, this.getIntent().getData(), this);
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        this.setIntent(intent);
     }
 }
